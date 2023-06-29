86a96725db147f40150ee846020fda36b1961692
wills_dev <godswillijezei@gmail.com>
2023-06-29 10:11:09 +0100

    onboard form

---
+++ pages/field/residents/index.tsx
@@ -0,0 +1,11 @@
+import { AppPage } from '@/shared/components/layouts/Types'
+import React from 'react'
+
+const ResidentsPage:AppPage = () => {
+  return (
+    <div>ResidentsPage</div>
+  )
+}
+
+export default ResidentsPage
+ResidentsPage.Layout ='Dashboard'
