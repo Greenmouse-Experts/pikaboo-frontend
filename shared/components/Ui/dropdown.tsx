21a715b7081637fcce830bb52a0d81eb5af137da
wills_dev <godswillijezei@gmail.com>
2023-07-24 13:10:43 +0100

    conflicts

--- shared/components/Ui/dropdown.tsx
+++ shared/components/Ui/dropdown.tsx
@@ -1,10 +1,10 @@
 "use client";
  
-import  ThemeProvider from "@material-tailwind/react";
+// import  ThemeProvider from "@material-tailwind/react/context/theme";
 import Menu from '@material-tailwind/react/components/Menu'
 import MenuHandler from "@material-tailwind/react/components/Menu";
 import MenuItem from "@material-tailwind/react/components/Menu";
 import MenuList from "@material-tailwind/react/components/Menu";
 import Button from "@material-tailwind/react/components/Button";
  
-export { ThemeProvider, Menu , MenuHandler, MenuItem, MenuList, Button };
+export { Menu , MenuHandler, MenuItem, MenuList, Button };
