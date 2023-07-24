74ed6032992f6cc9b3b914ad5b0a52b5d4437bb4
wills_dev <godswillijezei@gmail.com>
2023-07-24 12:51:57 +0100

    conflicts

--- next.config.js
+++ next.config.js
@@ -3,8 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   output: "export",
   images: {
-    loader: "cloudinary",
-    path: "",
+    // loader: "cloudinary",
+    // path: "",
     remotePatterns: [
       {
         protocol: "https",
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-const withMT = require('@material-tailwind/react/src/utils/withMT');
+const withMT = require("@material-tailwind/react/utils/withMT");
+
 module.exports = withMT({
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
@@ -8,6 +9,8 @@ module.exports = withMT({
     "./shared/**/*.{js,ts,jsx,tsx}",
     "./features/**/*.{js,ts,jsx,tsx}",
     "./hooks/**/*.{js,ts,jsx,tsx}",
+    "path-to-your-node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
+    "path-to-your-node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
